02a2626571124e0e24ede32374afe59e934aa01c
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2023-05-08 13:52:28 +0200

    Who won the most awards done

---
+++ Who won the most game awards/MostAwardsInTotal.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Read the CSV file into a DataFrame, replacing '—' with NaN values
+df = pd.read_csv('./data/cleaned_file.csv', na_values=['—'])
+
+# Calculate the frequency of each game receiving awards in different categories
+# Exclude the first two rows (header) and first two columns (category/organization)
+game_awards = df.iloc[2:, 2:].apply(pd.Series.value_counts).fillna(0)
+
+# Sum up the awards across different categories for each game
+total_awards = game_awards.sum(axis=1)
+
+# Find the game with the highest number of awards
+most_awarded_game = total_awards.idxmax()
+
+# Get the count of awards for the most awarded game
+most_awards_count = total_awards.loc[most_awarded_game]
+
+# Print the result
+print("The game with the most awards is:", most_awarded_game)
+print("Number of awards:", most_awards_count)
