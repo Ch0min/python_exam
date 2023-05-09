a30719201cd154c137b6c1af9e2b183e5099bf7a
oTuff <oscartuff@protonmail.com>
2023-05-09 13:41:14 +0200

    made plot look nice

--- critically_acclaimed/crittically_acclaimed.py
+++ critically_acclaimed/crittically_acclaimed.py
@@ -2,12 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("../data/Critically_acclaimed_games.csv")
-ax = df.plot(x='Title', y='Average score', kind='bar', figsize=(10, 6))
-ax.set_xlabel("Title", fontsize=10)
-ax.set_ylabel("Average score", fontsize=10)
-ax.set_ylim(89, 100)
 
-plt.xticks(rotation=60, fontsize=10)
+axes = df.plot(x='Title', y='Average score', kind='bar', figsize=(10, 6))
+axes.set_xlabel("Game Title", fontsize=10)
+axes.set_ylabel("Average score", fontsize=10)
+axes.set_ylim(89, 100)
+
+plt.xticks(rotation=90, fontsize=10)
 plt.title("Critically Acclaimed Games")
 plt.tight_layout()
 plt.show()
