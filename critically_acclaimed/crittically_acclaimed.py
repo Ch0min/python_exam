836e82e7fc633b5ce391679ef5f75a993972b4c8
oTuff <oscartuff@protonmail.com>
2023-05-10 16:38:33 +0200

    different colors for different platforms

--- critically_acclaimed/crittically_acclaimed.py
+++ critically_acclaimed/crittically_acclaimed.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 df = pd.read_csv("../data/Critically_acclaimed_games.csv")
+platform_color_map = {'NS': 'red', 'PS5': 'blue',
+                      'XSX': 'darkgreen', 'Win': 'yellow', 'XBO': 'limegreen', 'PS5, XSX': 'cyan', 'NS, XSX': 'brown'}
+colors = df['Platform(s)'].map(platform_color_map).tolist()
 
-axes = df.plot(x='Title', y='Average score', kind='bar', figsize=(10, 6))
+axes = df.plot(x='Title', y='Average score',
+               color=colors, kind='bar', figsize=(10, 6))
 axes.set_xlabel("Game Title", fontsize=10)
 axes.set_ylabel("Average score", fontsize=10)
 axes.set_ylim(89, 100)
+
+# Create a list of legend handles using platform_color_map
+legend_handles = [mpatches.Patch(color=color, label=platform)
+                  for platform, color in platform_color_map.items()]
+
+# Add the legend to the plot
+axes.legend(handles=legend_handles, loc='upper right')
 
 plt.xticks(rotation=90, fontsize=10)
 plt.title("Critically Acclaimed Games")
