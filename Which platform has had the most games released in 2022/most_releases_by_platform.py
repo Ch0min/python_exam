947fdbb9610d777046facdad59df8b4f96a96d56
R <90389727+notebrr@users.noreply.github.com>
2023-05-16 11:45:12 +0200

    risk it for the buscuit

--- Which platform has had the most games released in 2022/most_releases_by_platform.py
+++ Which platform has had the most games released in 2022/most_releases_by_platform.py
@@ -1,18 +1,21 @@
 import csv
 
-platform_counts = {}
+def get_most_releases_by_platform():
+    platform_counts = {}
 
-for filename in ["../data/January-March.csv", "../data/July-September.csv", "../data/October-December.csv"]:
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            platforms = row["Platform(s)"].split(", ")
-            for platform in platforms:
-                if platform in platform_counts:
-                    platform_counts[platform] += 1
-                else:
-                    platform_counts[platform] = 1
+    for filename in ["../data/January-March.csv", "../data/July-September.csv", "../data/October-December.csv"]:
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                platforms = row["Platform(s)"].split(", ")
+                for platform in platforms:
+                    if platform in platform_counts:
+                        platform_counts[platform] += 1
+                    else:
+                        platform_counts[platform] = 1
 
-most_releases = max(platform_counts, key=platform_counts.get)
-count = platform_counts[most_releases]
-print("The platform with the most releases is", most_releases, "with", count, "games released.")
+    most_releases = max(platform_counts, key=platform_counts.get)
+    count = platform_counts[most_releases]
+    return [most_releases, count]
+
+
