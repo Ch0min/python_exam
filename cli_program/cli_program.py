76ea9a92380b814bfd3c097f159cc4ac401d64ba
Mark Lundgaard <marklundgaard7@gmail.com>
2023-05-16 12:03:02 +0200

    Cli Updated with subprocess

--- cli_program/cli_program.py
+++ cli_program/cli_program.py
@@ -1,29 +1,77 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
+import subprocess
+# The subprocess library in Python allows you to spawn and manage subprocesses,
+# interact with their input/output, and handle errors, providing powerful capabilities
+# for executing external commands and automating tasks.
+
+# Define the CLI menu options
+menu_options = {
+    1: {
+        "name": "Which month had the most and least released games in 2022?",
+        "scripts": [
+            "../Questions/Which_month_had_the_most_and_least_released_games_in_2022/month_released_most.py",
+            "../Questions/Which_month_had_the_most_and_least_released_games_in_2022/month_released_less.py"
+        ]
+    },
+    2: {
+        "name": "Which Game Dev Publisher had released the most games in 2022?",
+        "scripts": [
+            "../Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py"
+        ]
+    },
+    3: {
+        "name": "Which gaming platform has had the most games released in 2022?",
+        "scripts": [
+            "../Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py"
+        ]
+    },
+    4: {
+        "name": "Which game had won the most awards in 2022?",
+        "scripts": [
+            "../Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsByCategory.py",
+            "../Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsInTotal.py"
+        ]
+    },
+    5: {
+        "name": "Which genre was the most popular among the released games in 2022?",
+        "scripts": ["missing"]
+    },
+    6: {
+        "name": "Show a graph for the most released gaming genre in 2022.",
+        "scripts": [
+            "../Questions/Show_a_graph_for_the_most_released_gaming_genre_in_2022/PopularGenreGraph.py"
+        ]
+    },
+    7: {
+        "name": "Show a graph for the Critically Acclaimed Games with the best rating.",
+        "scripts": [
+            "../Questions/Show_a_graph_for_the_Critically_Acclaimed_Games_with_the_best_rating/critically_acclaimed.py"
+        ]
+    },
+    8: {
+        "name": "Is there a connection between genre and platform?",
+        "scripts": ["helloworld.py"]
+    },
+    9: {
+        "name": "Which game will we recommend based on genre and platform (possibly rating too)?",
+        "scripts": ["calculate.py"]
+    }
+}
 
 # Define the CLI menu options
 parser = argparse.ArgumentParser(description="Select a QUESTION:")
-parser.add_argument("script", type=int, nargs="?", choices=[1, 2, 3, 4, 5, 6, 7, 8, 9], default=None,
-                    help="Select a QUESTION between 1 and 9 or use the -m or --menu to show the QUESTIONS.")
+parser.add_argument("script", type=int, nargs="?", choices=list(menu_options.keys()), default=None,
+                    help=f"Select a QUESTION between {menu_options.keys()} or use the -m or --menu to show the QUESTIONS.")
 parser.add_argument("-m", "--menu", action="store_true", help="display the CLI MENU options")
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 if args.menu:
-    print("1 - Which month had the most and least released games in 2022?")
-    print("2 - Which Game Dev Publisher had released the most games in 2022?")
-    print("3 - Which gaming platform has had the most games released in 2022?")
-    print("4 - Which game had won the most awards in 2022?")
-    print("5 - Which genre was the most popular among the released games in 2022?")
-    print("6 - Show a graph for the most released gaming genre in 2022.")
-    print("7 - Show a graph for the Critically Acclaimed Games with the best rating.")
-    print("8 - Is there a connection between genre and platform?")
-    print("9 - Which game will we recommend based on genre and platform (possibly rating too)?")
-
-
+    for key, option in menu_options.items():
+        print(f"{key} - {option['name']}")
 else:
     if args.script is None:
         print("##### #    # ###### #    # ###### #    #     ###### #     #      #      #     #")
@@ -33,28 +81,14 @@ else:
         print("#       #      #    #    # #    # #   ##     #       #   #   #       #  #     #")
         print("#       #      #    #    # ###### #    #     ###### #     # #         # #     #")
         print("                     PYTHON EXAM 2023 - GAMES IN 2022                          ")
-        print("Please select a QUESTION (./cli_program.py (1-9)) or use the -m or --menu OPTION to display the MENU.   ")
+        print(
+            "Please select a QUESTION (./cli_program.py (1-9)) or use the -m or --menu OPTION to display the MENU.   ")
     else:
         # Run the selected script
-        if args.script == 1:
-            os.system("python3 ../Questions/Which_month_had_the_most_and_least_released_games_in_2022/month_released_most.py")
-            print("\t")
-            os.system("python3 ../Questions/Which_month_had_the_most_and_least_released_games_in_2022/month_released_less.py")
-        elif args.script == 2:
-            os.system("python3 ../Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py")
-        elif args.script == 3:
-            os.system("python3 ../Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py")
-        elif args.script == 4:
-            os.system("python3 ../Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsByCategory.py")
-            print("\t")
-            os.system("python3 ../Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsInTotal.py")
-        elif args.script == 5:
-            os.system("python3 missing")
-        elif args.script == 6:
-            os.system("python3 ../Questions/Show_a_graph_for_the_most_released_gaming_genre_in_2022/PopularGenreGraph.py")
-        elif args.script == 7:
-            os.system("python3 ../Questions/Show_a_graph_for_the_Critically_Acclaimed_Games_with_the_best_rating/critically_acclaimed.py")
-        elif args.script == 8:
-            os.system("python3 helloworld.py")
-        elif args.script == 9:
-            os.system("python3 calculate.py")
+        if args.script in menu_options:
+            scripts = menu_options[args.script]["scripts"]
+            for script in scripts:
+                subprocess.run(["python3", script])
+                print("\t")
+        else:
+            print("Invalid script number. Please select a valid QUESTION.")
---
+++ cli_program/cli_program_test.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+#
+# import argparse
+# import subprocess
+#
+# # Define the CLI menu options
+# parser = argparse.ArgumentParser(description="Select a QUESTION:")
+# parser.add_argument("script", type=int, nargs="?", choices=[1, 2, 3, 4, 5, 6, 7, 8, 9], default=None,
+#                     help="Select a QUESTION between 1 and 9 or use the -m or --menu to show the QUESTIONS.")
+# parser.add_argument("-m", "--menu", action="store_true", help="display the CLI MENU options")
+#
+# # Parse the command-line arguments
+# args = parser.parse_args()
+#
+# if args.menu:
+#     print("1 - Which month had the most and least released games in 2022?")
+#     print("2 - Which Game Dev Publisher had released the most games in 2022?")
+#     print("3 - Which gaming platform has had the most games released in 2022?")
+#     print("4 - Which game had won the most awards in 2022?")
+#     print("5 - Which genre was the most popular among the released games in 2022?")
+#     print("6 - Show a graph for the most released gaming genre in 2022.")
+#     print("7 - Show a graph for the Critically Acclaimed Games with the best rating.")
+#     print("8 - Is there a connection between genre and platform?")
+#     print("9 - Which game will we recommend based on genre and platform (possibly rating too)?")
+#
+#
+# else:
+#     if args.script is None:
+#         print("##### #    # ###### #    # ###### #    #     ###### #     #      #      #     #")
+#         print("#   #  #  #    #    #    # #    # ##   #     #       #   #      # #     ##   ##")
+#         print("#   #   ##     #    #    # #    # # #  #     #        # #      #   #    # # # #")
+#         print("####    #      #    ###### #    # #  # #     ######    #      #######   #  #  #")
+#         print("#       #      #    #    # #    # #   ##     #       #   #   #       #  #     #")
+#         print("#       #      #    #    # ###### #    #     ###### #     # #         # #     #")
+#         print("                     PYTHON EXAM 2023 - GAMES IN 2022                          ")
+#         print(
+#             "Please select a QUESTION (./cli_program.py (1-9)) or use the -m or --menu OPTION to display the MENU.   ")
+#     else:
+#         # Run the selected script
+#         if args.script == 1:
+#             subprocess.run(["python3",
+#                             "../Questions/Which_month_had_the_most_and_least_released_games_in_2022/month_released_most.py"])
+#             print("\t")
+#             subprocess.run(["python3",
+#                             "../Questions/Which_month_had_the_most_and_least_released_games_in_2022/month_released_less.py"])
+#         elif args.script == 2:
+#             subprocess.run(["python3",
+#                             "../Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py"])
+#         elif args.script == 3:
+#             subprocess.run(["python3",
+#                             "../Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py"])
+#         elif args.script == 4:
+#             subprocess.run(
+#                 ["python3", "../Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsByCategory.py"])
+#             print("\t")
+#             subprocess.run(["python3", "../Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsInTotal.py"])
+#         elif args.script == 5:
+#             subprocess.run(["python3", "missing"])
+#         elif args.script == 6:
+#             subprocess.run(["python3",
+#                             "../Questions/Show_a_graph_for_the_most_released_gaming_genre_in_2022/PopularGenreGraph.py"])
+#         elif args.script == 7:
+#             subprocess.run(["python3",
+#                             "../Questions/Show_a_graph_for_the_Critically_Acclaimed_Games_with_the_best_rating/critically_acclaimed.py"])
+#         elif args.script == 8:
+#             subprocess.run(["python3", "helloworld.py"])
+#         elif args.script == 9:
+#             subprocess.run(["python3", "calculate.py"])
