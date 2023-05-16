99305d68159dab05ca7c7922969ef77aed4b5e5c
R <90389727+notebrr@users.noreply.github.com>
2023-05-16 12:33:44 +0200

    risk it for the buscuit

--- Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py
+++ Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py
@@ -1,19 +1,18 @@
 import csv
 
-def most_releases_by_platform():
-    game_counts = {}
+game_counts = {}
 
-    for filename in ["../../data/January-March.csv", "../../April-June.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
-        with open(filename) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                developers = row["Developer(s)"].split("; ")
-                for dev in developers:
-                    if dev in game_counts:
-                        game_counts[dev] += 1
-                    else:
-                        game_counts[dev] = 1
+for filename in ["../../data/January-March.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            developers = row["Developer(s)"].split("; ")
+            for dev in developers:
+                if dev in game_counts:
+                    game_counts[dev] += 1
+                else:
+                    game_counts[dev] = 1
 
-    most_releases = max(game_counts, key=game_counts.get)
-    count = game_counts[most_releases]
-    return [most_releases, count]
+most_releases = max(game_counts, key=game_counts.get)
+count = game_counts[most_releases]
+print("Most releases: "+str(most_releases) + ", released "+ str(count) + " times.")
