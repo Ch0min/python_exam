6e6119f6a6394b1864ae55199d63d8ef186544b2
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2023-05-22 11:22:35 +0200

    comments and cleanup

--- Questions/Connection_Between_Genre_And_Platform/ConnectionGraph.py
+++ Questions/Connection_Between_Genre_And_Platform/ConnectionGraph.py
@@ -7,7 +7,7 @@ df_apr_jun = pd.read_csv('../data/April-June-CLEAN.csv')
 df_jul_sep = pd.read_csv('../data/July-September-CLEAN.csv')
 df_oct_dec = pd.read_csv('../data/October-December-CLEAN.csv')
 
-# Concat all dataframes into one and extract the "Month" and "Title" columns:
+# Her sætter vi vores 4 csv filer sammen ved hjælp af pandas concat og hiver fat i platforms og genre kolunnerne
 df = pd.concat([df_jan_mar[['Platform(s)', 'Genre(s)']],
                 df_apr_jun[['Platform(s)', 'Genre(s)']],
                 df_jul_sep[['Platform(s)', 'Genre(s)']],
@@ -16,10 +16,10 @@ df = pd.concat([df_jan_mar[['Platform(s)', 'Genre(s)']],
 df['Platform(s)'] = df['Platform(s)'].str.split(', ')
 df = df.explode('Platform(s)')
 
-# Handle missing values in the genre column
+# Her sørger vi for at bruge fillna til at håndtere de steder hvor der ikke er en genre tilstede
 df['Genre(s)'].fillna('Unknown', inplace=True)
 
-# Plot the bar chart
+# Her bruger vi matplob til at visuelisere vores data
 plt.figure(figsize=(12, 6))
 sns.countplot(data=df, x='Genre(s)', hue='Platform(s)')
 plt.xticks(rotation=45)
--- Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsByCategory.py
+++ Questions/Which_game_had_won_the_most_awards_in_2022/MostAwardsByCategory.py
@@ -1,47 +1,46 @@
 import pandas as pd
 
 try:
-    # Read the CSV file
+    # Vi læser vores csv fil ved at bruge panda
     data = pd.read_csv('../data/cleaned_file.csv')
 
-    # Category to check
+    # Vi definerer den category som vi skal lave en graf ud fra
     category = 'The Game Awards 2022 December 8, 2022[3]'
 
-    # Filter the DataFrame to include only the relevant columns for "The Game Awards 2022"
+    # Her sørger vi for at vi kun kigger på den katogorien som vi har defineret ovenover
     game_awards_2022 = data[["Category/Organization", category]]
 
-    # Create an empty dictionary to store the game titles and their award counts
+    # Her laver vi et tomt dictionary til at gemme spil titlerne og deres mængde af awards
     game_awards_count = {}
 
-    # Iterate over each row in the DataFrame
+    # Her looper vi over hver række i vores datasæt
     for index, row in game_awards_2022.iterrows():
         try:
-            # Get the game titles (including mixed information)
+            # Her får vi titlerne på spillet
             game_titles = row["The Game Awards 2022 December 8, 2022[3]"].split(',')
 
-            # Iterate over each game title
+            # Her looper vi henover hvert spil title
             for title in game_titles:
-                # Strip any leading/trailing whitespaces
+                # Hvis der er noget whitespace eller tomme felter, så bruger vi strip() til at fjerne det
                 title = title.strip()
 
-                # Check if the game title already exists in the dictionary
+                # Her tjekker vi om spillet allerede eksisterer i vores dictionary
                 if title in game_awards_count:
-                    # Increment the award count for the existing game title
+                    # Vi ligger spillet oveni det nuværende count
                     game_awards_count[title] += 1
                 else:
-                    # Create a new entry for the game title with an initial award count of 1
+                    # Hvis spillet ikke findes i vores dictionary oprettes det her og tildeles count
                     game_awards_count[title] = 1
         except KeyError:
             print("Error: The specified category does not exist in the DataFrame.")
             break
 
-    # Find the maximum award count
+    # Her finder vi det spil med flest awards
     max_awards = max(game_awards_count.values())
 
-    # Find all the games with the maximum award count
+    # Her tjekker vi om der er flere spil med samme mængde awards i toppen
     games_with_max_awards = [game_title for game_title, award_count in game_awards_count.items() if award_count == max_awards]
 
-    # Print the games with the most awards won
     print("The game(s) that won the most awards at " + category + ":")
     for game in games_with_max_awards:
         print(game)
