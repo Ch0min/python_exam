54f5f55bf19d956711f791e444d614e0222b50d7
R <90389727+notebrr@users.noreply.github.com>
2023-05-30 11:49:17 +0200

    Use pandas instead of csv

--- Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
+++ Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
@@ -1,21 +1,19 @@
 import csv
 
-def get_most_releases_by_platform():
-    platform_counts = {}
 
-    for filename in ["../../data/January-March.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
-        with open(filename) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                platforms = row["Platform(s)"].split(", ")
-                for platform in platforms:
-                    if platform in platform_counts:
-                        platform_counts[platform] += 1
-                    else:
-                        platform_counts[platform] = 1
+platform_counts = {}
 
-    most_releases = max(platform_counts, key=platform_counts.get) # max returnere den største værdi i et array
-    return platform_counts.get
+for filename in ["../../data/January-March.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            platforms = row["Platform(s)"].split(", ")
+            for platform in platforms:
+                if platform in platform_counts:
+                    platform_counts[platform] += 1
+                else:
+                    platform_counts[platform] = 1
 
-
-print(get_most_releases_by_platform())
+most_releases = max(platform_counts, key=platform_counts.get) # max returnere den største værdi i et array
+##platform_counts.get
+print(most_releases)
