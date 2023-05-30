5015133ab311e35ea528e57590ec547c0639da2b
R <90389727+notebrr@users.noreply.github.com>
2023-05-30 11:48:32 +0200

    Use pandas instead of csv

--- Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py
+++ Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py
@@ -1,17 +1,13 @@
-import pandas as pd
-import numpy as np
+import csv
 
 game_counts = {}
-file_paths = ["../../data/January-March.csv", "../../data/April-June.csv", "../../data/July-September.csv",
-              "../../data/October-December.csv"]
 
-for file_path in file_paths:
-    df = pd.read_csv(file_path)
-    developers = df["Developer(s)"].str.split("; ")
-
-    for dev_list in developers:
-        if isinstance(dev_list, list):
-            for dev in dev_list:
+for filename in ["../../data/January-March.csv", "../../data/April-June.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            developers = row["Developer(s)"].split("; ")
+            for dev in developers:
                 if dev in game_counts:
                     game_counts[dev] += 1
                 else:
@@ -19,4 +15,4 @@ for file_path in file_paths:
 
 most_releases = max(game_counts, key=game_counts.get)
 count = game_counts[most_releases]
-print("Most releases: " + str(most_releases) + ", released " + str(count) + " times.")
+print("Most releases: "+str(most_releases) + ", released "+ str(count) + " times.")
--- Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
+++ Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
@@ -1,23 +1,21 @@
-import pandas as pd
+import csv
 
 def get_most_releases_by_platform():
     platform_counts = {}
 
-    file_paths = ["../../data/January-March.csv", "../../data/July-September.csv", "../../data/October-December.csv"]
-
-    for file_path in file_paths:
-        df = pd.read_csv(file_path)
-        platforms = df["Platform(s)"].str.split(", ")
-        for platform_list in platforms:
-            # Check if platform_list is not NaN or float
-            if isinstance(platform_list, list):
-                for platform in platform_list:
+    for filename in ["../../data/January-March.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                platforms = row["Platform(s)"].split(", ")
+                for platform in platforms:
                     if platform in platform_counts:
                         platform_counts[platform] += 1
                     else:
                         platform_counts[platform] = 1
 
-    most_releases = max(platform_counts, key=platform_counts.get)
-    print(most_releases)
+    most_releases = max(platform_counts, key=platform_counts.get) # max returnere den største værdi i et array
+    return platform_counts.get
 
-get_most_releases_by_platform()
+
+print(get_most_releases_by_platform())
