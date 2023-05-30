e5e508266fe62643769174644f3e889f881394d0
R <90389727+notebrr@users.noreply.github.com>
2023-05-30 12:17:57 +0200

    Use pandas instead of csv

--- Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py
+++ Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/most_game_releases_2022_by_company.py
@@ -1,13 +1,25 @@
+import pandas as pd
 import csv
+
+df_jan_mar = pd.read_csv('../../data/January-March-CLEAN.csv')
+df_apr_jun = pd.read_csv('../../data/April-June-CLEAN.csv')
+df_jul_sep = pd.read_csv('../../data/July-September-CLEAN.csv')
+df_oct_dec = pd.read_csv('../../data/October-December-CLEAN.csv')
+
+df = pd.concat([df_jan_mar[['Platform(s)', 'Genre(s)']],
+                df_apr_jun[['Platform(s)', 'Genre(s)']],
+                df_jul_sep[['Platform(s)', 'Genre(s)']],
+                df_oct_dec[['Platform(s)', 'Genre(s)']]])
+
+df['Genre(s)'].fillna('Unknown', inplace=True)
 
 game_counts = {}
 
-for filename in ["../../data/January-March.csv", "../../data/April-June.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            developers = row["Developer(s)"].split("; ")
-            for dev in developers:
+for df_chunk in [df_jan_mar, df_apr_jun, df_jul_sep, df_oct_dec]:
+    developers = df_chunk["Developer(s)"].str.split("; ")
+    for dev_list in developers:
+        if pd.notnull(dev_list) and dev_list:
+            for dev in dev_list:
                 if dev in game_counts:
                     game_counts[dev] += 1
                 else:
@@ -15,4 +27,4 @@ for filename in ["../../data/January-March.csv", "../../data/April-June.csv", ".
 
 most_releases = max(game_counts, key=game_counts.get)
 count = game_counts[most_releases]
-print("Most releases: "+str(most_releases) + ", released "+ str(count) + " times.")
+print("Most releases: " + str(most_releases) + ", released " + str(count) + " times.")
