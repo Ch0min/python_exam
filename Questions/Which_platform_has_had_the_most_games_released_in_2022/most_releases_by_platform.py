33d34988868da5ae3e8c22f27777ec65f4116554
R <90389727+notebrr@users.noreply.github.com>
2023-05-30 11:56:08 +0200

    Use pandas instead of csv

--- Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
+++ Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
@@ -1,19 +1,25 @@
-import csv
+import pandas as pd
 
+df_jan_mar = pd.read_csv('../../data/January-March-CLEAN.csv')
+df_apr_jun = pd.read_csv('../../data/April-June-CLEAN.csv')
+df_jul_sep = pd.read_csv('../../data/July-September-CLEAN.csv')
+df_oct_dec = pd.read_csv('../../data/October-December-CLEAN.csv')
+
+df = pd.concat([df_jan_mar[['Platform(s)', 'Genre(s)']],
+                df_apr_jun[['Platform(s)', 'Genre(s)']],
+                df_jul_sep[['Platform(s)', 'Genre(s)']],
+                df_oct_dec[['Platform(s)', 'Genre(s)']]])
+
+df['Genre(s)'].fillna('Unknown', inplace=True)
 
 platform_counts = {}
 
-for filename in ["../../data/January-March.csv", "../../data/July-September.csv", "../../data/October-December.csv"]:
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            platforms = row["Platform(s)"].split(", ")
-            for platform in platforms:
-                if platform in platform_counts:
-                    platform_counts[platform] += 1
-                else:
-                    platform_counts[platform] = 1
+for platforms in df.loc[pd.notnull(df['Platform(s)']), 'Platform(s)'].str.split(", "):
+    for platform in platforms:
+        if platform in platform_counts:
+            platform_counts[platform] += 1
+        else:
+            platform_counts[platform] = 1
 
-most_releases = max(platform_counts, key=platform_counts.get) # max returnere den største værdi i et array
-##platform_counts.get
+most_releases = max(platform_counts, key=platform_counts.get)
 print(most_releases)
