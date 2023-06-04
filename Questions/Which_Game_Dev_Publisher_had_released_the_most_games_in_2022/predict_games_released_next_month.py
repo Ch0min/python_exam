95d3bf577610ce0287e79d0949954e5d9f2854c2
R <90389727+notebrr@users.noreply.github.com>
2023-06-04 21:23:51 +0200

    forgot to push

--- Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/predict_games_released_next_month.py
+++ Questions/Which_Game_Dev_Publisher_had_released_the_most_games_in_2022/predict_games_released_next_month.py
@@ -10,41 +10,41 @@ data = pd.read_csv('../../data/January-March.csv')
 
 # Få antal spil der er blevet udgivet af alle spiludviklere i de måneder der er valgt
 developer_counts = data.loc[~data['Month'].str.lower().str.contains('april'), 'Developer(s)'].str.strip().value_counts().to_dict()
-
 # Gør data klar til at træne model
 x = np.array([[developer_counts[developer]] for developer in developer_counts.keys()])
+print(x)
 y = np.array(list(developer_counts.values()))
 
 # Vi bruger cross-validation til at få et estimat om vores model er præcis.
 model = make_pipeline(StandardScaler(), Ridge(alpha=0.1))
-scores = cross_val_score(model, x, y, cv=5, scoring='r2')
-mean_accuracy = np.mean(scores)
 
 # Træn model med data (lineær, derfor x, og y)
 model.fit(x, y)
 
-# Lav forudsigelser for alle spiludviklere
-predictions = {developer: round(model.predict([[developer_counts[developer]]])[0]) for developer in developer_counts.keys()}
+predictions = {}
 
-# Vi printer en forudsigelse af mængden af spil fra en given udgiver.
+for developer in developer_counts:
+    #list i en liste fordi predict kræver 2d array
+    count = round(model.predict([[developer_counts[developer]]])[0])  # Forudsig antallet af spil udgivet af en spiludvikler
+    predictions[developer] = count  # Gem forudsigelsen i dictionaryen predictions med udvikleren som nøgle
+
+# Print forudsigelserne
 for developer, count in predictions.items():
-    print(f"{developer} is predicted to release {count} games in April.")
-print(f"Mean Accuracy: {mean_accuracy}")
+    print(f"{developer} forventes at udgive {count} spil i april.")
 
 # Sorter spiludviklerne og predicted_counts så den viser antallet af spil spiludviklerne har udgivet, fra flest udgivede og ned af til færrest udgivede
 # Få de 10 spiludviklere som har udgivet flest spil og antallet af spil de har udgivet
-sorted_data = sorted(predictions.items(), key=lambda x: x[1], reverse=True)
-developers = [d[0] for d in sorted_data[:10]]
-predicted_counts = [d[1] for d in sorted_data[:10]]
+df = pd.DataFrame({'developer': list(predictions.keys()), 'predicted_counts': list(predictions.values())})
 
-# Lav graf
+sorted_data = df.sort_values(by='predicted_counts', ascending=False).head(10)
+
 plt.figure(figsize=(10, 6))
-plt.bar(developers, predicted_counts)
+plt.bar(sorted_data['developer'], sorted_data['predicted_counts'])
 plt.xlabel("Game Developer")
-plt.ylabel("Number of Games Predicted")
-plt.title("Predicted Number of Games Released by Game Developers in April")
+plt.ylabel("Number of games predicted")
+plt.title("Predicted number of games released by game gevelopers in April")
 
-plt.xticks(rotation=45, ha='right', fontsize=6)
+plt.xticks(rotation=45, fontsize=6)
 
-plt.tight_layout()
+plt.tight_layout() # fjerener whitespace
 plt.show()
--- Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
+++ Questions/Which_platform_has_had_the_most_games_released_in_2022/most_releases_by_platform.py
@@ -1,25 +1,16 @@
 import pandas as pd
+import csv
 
 df_jan_mar = pd.read_csv('../../data/January-March-CLEAN.csv')
 df_apr_jun = pd.read_csv('../../data/April-June-CLEAN.csv')
 df_jul_sep = pd.read_csv('../../data/July-September-CLEAN.csv')
 df_oct_dec = pd.read_csv('../../data/October-December-CLEAN.csv')
 
-df = pd.concat([df_jan_mar[['Platform(s)', 'Genre(s)']],
-                df_apr_jun[['Platform(s)', 'Genre(s)']],
-                df_jul_sep[['Platform(s)', 'Genre(s)']],
-                df_oct_dec[['Platform(s)', 'Genre(s)']]])
+concatenated_df = pd.concat([df_jan_mar, df_apr_jun, df_jul_sep, df_oct_dec])
+developers = concatenated_df["Platform(s)"]
 
-df['Genre(s)'].fillna('Unknown', inplace=True)
+game_counts = developers.value_counts()
+most_releases = game_counts.idxmax()
+count = game_counts[most_releases]
 
-platform_counts = {}
-
-for platforms in df.loc[pd.notnull(df['Platform(s)']), 'Platform(s)'].str.split(", "):
-    for platform in platforms:
-        if platform in platform_counts:
-            platform_counts[platform] += 1
-        else:
-            platform_counts[platform] = 1
-
-most_releases = max(platform_counts, key=platform_counts.get)
-print(most_releases)
+print(f"Platformw ith most releases: {most_releases}, released {count} times.")
